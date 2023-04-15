5f955b324bba242f92c99cc3f25733fcf235ea21
David Holmes <david.holmes@kpnmail.nl>
2023-04-15 17:17:14 +0100

    Added continues spectral analysis to matlab script
    need to add log file for data further DSP steps

--- Matlab_interface/xmegacomtest.m
+++ Matlab_interface/xmegacomtest.m
@@ -7,9 +7,16 @@ COM = 'COM12';
 xmega = serialport(COM,115200,'Timeout',10);
 flush(xmega);
 
-write(xmega, 1, "uint8");                                                    %Send start sign
+while 1
+    
+    write(xmega, 1, "uint8");                                                    %Send start sign
+    
+    while xmega.NumBytesAvailable < (N*4)
+        %Do nothing 
+    end
 
-spectrum = read(xmega, N, "single");                                         %Read spectrum
-checksum = read(xmega, 1, "single");
-
-stem(spectrum)                                                               %Plot spectrum
+    spectrum = read(xmega, N, "single");                                        %Read spectrum
+    checksum = read(xmega, 1, "single");                                        %Read checksum
+    stem(spectrum);                                                             %Plot spectrum
+    drawnow
+end
